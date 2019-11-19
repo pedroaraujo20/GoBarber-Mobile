bc781e391794f7ac70601a8535572a49497cc28b
Pedro Araujo <pedro-foa@hotmail.com>
2019-11-19 14:12:08 -0300

    configured reactotron redux reduxsaga

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -26,3 +26,13 @@ export const FormInput = styled(Input)`
 export const SubmitButton = styled(Button)`
   margin-top: 5px;
 `;
+
+export const SignLink = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+
+export const SignLinkText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+`;
